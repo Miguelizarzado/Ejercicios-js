487cf161ad760773c40ee6e6b2f924aa1c61d786
Miguelizarzado <124523459+Miguelizarzado@users.noreply.github.com>
2023-02-24 16:27:28 -0400

    Add files via upload

---
+++ Ejercicio2/Ejercicio-2.js
@@ -0,0 +1,14 @@
+const miguel = [ 
+    "Miguel Lizarzado", 
+    26, 
+    true, 
+    new Date("feb 8 1997"), 
+    libro = {
+        titulo: "It", 
+        autor: "Stephen King", 
+        fecha: new Date("1986"), 
+        url: "https://es.wikipedia.org/wiki/It_(novela)"
+        } 
+    ]
+
+console.log(miguel)
